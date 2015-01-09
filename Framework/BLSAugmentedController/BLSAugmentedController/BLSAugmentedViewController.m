81d28eb8803d13b1478281d471e3a28b870b8893
Piotr Tobolski <piotr.tobolski@blstream.com>
2015-01-09 15:55:04 +0100

    AR first draft

--- Framework/BLSAugmentedController/BLSAugmentedController/BLSAugmentedViewController.m
+++ Framework/BLSAugmentedController/BLSAugmentedController/BLSAugmentedViewController.m
@@ -13,6 +13,7 @@
 #import <CoreMotion/CoreMotion.h>
 #import <MapKit/MapKit.h>
 
+#pragma mark - Class Extension
 
 @interface BLSAugmentedViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
 
@@ -25,9 +26,61 @@
 
 @property (nonatomic, weak) MKMapView *mapView;
 @property (nonatomic, weak) UIView *vrView;
+@property (nonatomic, weak) AVCaptureDevice *captureDevice;
+@property (nonatomic, weak) AVCaptureSession *captureSession;
+@property (nonatomic, weak) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+
+@property (nonatomic, weak) NSTimer *refreshARAnnotationViewsTimer;
 
 @end
 
+#pragma mark - Helper Functions
+
+static inline double BLSDegreesToRadians(double degrees) {
+    return (degrees * M_PI / 180.0);
+}
+
+static inline double BLSNormalizeRadians(double angle) {
+    while (angle < 0) {
+        angle += 2 * M_PI;
+    }
+    return fmod(angle, 2 * M_PI);
+}
+
+double BLSBearingBetweenCoordinates(CLLocationCoordinate2D coordinate1, CLLocationCoordinate2D coordinate2) {
+    double lat1 = BLSDegreesToRadians(coordinate1.latitude);
+    double lon1 = BLSDegreesToRadians(coordinate1.longitude);
+    double lat2 = BLSDegreesToRadians(coordinate2.latitude);
+    double lon2 = BLSDegreesToRadians(coordinate2.longitude);
+    double deltaLon = lon2 - lon1;
+    double k = cos(lat2);
+    double x = sin(deltaLon) * k;
+    double y = cos(lat1) * sin(lat2) - sin(lat1) * k * cos(deltaLon);
+    double result = atan2(x, y);
+    return BLSNormalizeRadians(result);
+}
+
+CLLocationDistance BLSDistanceBetweenCoordinated(CLLocationCoordinate2D coordinate1, CLLocationCoordinate2D coordinate2) {
+    double lat1 = BLSDegreesToRadians(coordinate1.latitude);
+    double lon1 = BLSDegreesToRadians(coordinate1.longitude);
+    double lat2 = BLSDegreesToRadians(coordinate2.latitude);
+    double lon2 = BLSDegreesToRadians(coordinate2.longitude);
+    double deltaLat = lat2 - lat1;
+    double deltaLon = lon2 - lon1;
+    double p = sin(deltaLat/2);
+    double q = sin(deltaLon/2);
+    double a = p * p + q * q * cos(lat1) * cos(lat2);
+    double c = 2 * asin(sqrt(a));
+    double R = 6371000; //earth radius
+    return c * R;
+}
+
+static inline CGPoint BLSCGPointAdd(CGPoint p1, CGPoint p2)
+{
+    return (CGPoint){p1.x + p2.x, p1.y + p2.y};
+}
+
+#pragma mark -
 
 @implementation BLSAugmentedViewController
 
@@ -59,6 +112,7 @@
     } else if (self.style == BLSAugmentedViewControllerStyleVR) {
         [self loadVRView];
     }
+    [self.locationManager requestWhenInUseAuthorization];
     
     [self.locationManager startUpdatingLocation];
     [self.locationManager startUpdatingHeading];
@@ -89,6 +143,7 @@
     if (_locationManager == nil) {
         _locationManager = [[CLLocationManager alloc] init];
         _locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation;
+        _locationManager.headingFilter = kCLHeadingFilterNone;
         _locationManager.delegate = self;
     }
     return _locationManager;
@@ -97,8 +152,8 @@
 - (CMMotionManager *)motionManager {
     if (_motionManager == nil) {
         _motionManager = [[CMMotionManager alloc] init];
-        _motionManager.deviceMotionUpdateInterval = 1/30;
-        
+        _motionManager.deviceMotionUpdateInterval = 1.0/30.0;
+        _motionManager.showsDeviceMovementDisplay = YES;
     }
     return _motionManager;
 }
@@ -143,10 +198,15 @@
 }
 
 - (BLSAugmentedAnnotationView *)dequeueReusableAnnotationViewWithIdentifier:(NSString *)identifier {
-    for (BLSAugmentedAnnotationView *view in self.reusableViews) {
-        if ([view.reuseIdentifier isEqualToString:identifier]) {
-            [self.reusableViews removeObject:view];
-            return view;
+    if (self.style == BLSAugmentedViewControllerStyleMap) {
+        return (BLSAugmentedAnnotationView *)[self.mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+    } else {
+        for (BLSAugmentedAnnotationView *view in self.reusableViews) {
+            if ([view.reuseIdentifier isEqualToString:identifier]) {
+                BLSAugmentedAnnotationView *viewToReuse = view;
+                [self.reusableViews removeObject:view];
+                return viewToReuse;
+            }
         }
     }
     return nil;
@@ -155,6 +215,7 @@
 - (void)loadMapView {
     [self.mapView removeFromSuperview];
     [self.vrView removeFromSuperview];
+    [self.refreshARAnnotationViewsTimer invalidate];
     
     MKMapView *mapView = [[MKMapView alloc] initWithFrame:self.view.bounds];
     self.mapView = mapView;
@@ -178,18 +239,109 @@
     
     [self.view insertSubview:vrView atIndex:0];
     
-    NSError *error = nil;
+    
     AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    self.captureDevice = captureDevice;
+    
+    NSError *error = nil;
     AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
     if (!input) {
         NSLog(@"%@", [error localizedDescription]);
         return;
     }
+    
+    
     AVCaptureSession *session = [[AVCaptureSession alloc] init];
+    self.captureSession = session;
+    
     [session addInput:input];
     
-    //TODO: AVCaptureVideoPreviewLayer
     
+    AVCaptureVideoPreviewLayer *videoPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    self.videoPreviewLayer = videoPreviewLayer;
+    
+    videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+    videoPreviewLayer.frame = vrView.bounds;
+    [vrView.layer addSublayer:videoPreviewLayer];
+    
+    [self configureVideoOrientation];
+    
+    //move to appear method
+    [session startRunning];
+    [self.motionManager startDeviceMotionUpdates];
+    
+    self.refreshARAnnotationViewsTimer = [NSTimer scheduledTimerWithTimeInterval:1.0/30.0 target:self selector:@selector(refreshARAnnotationViewsTimerTimeout:) userInfo:nil repeats:YES];
+}
+
+- (void)configureVideoOrientation {
+    UIDeviceOrientation deviceOrientation = [[UIDevice currentDevice] orientation];
+    if (deviceOrientation == UIDeviceOrientationLandscapeLeft) {
+        self.videoPreviewLayer.connection.videoOrientation = AVCaptureVideoOrientationLandscapeRight;
+    } else if (deviceOrientation == UIDeviceOrientationLandscapeRight) {
+        self.videoPreviewLayer.connection.videoOrientation = AVCaptureVideoOrientationLandscapeLeft;
+    } else if (deviceOrientation == UIDeviceOrientationPortrait) {
+        self.videoPreviewLayer.connection.videoOrientation = AVCaptureVideoOrientationPortrait;
+    } else if (deviceOrientation == UIDeviceOrientationPortraitUpsideDown) {
+        self.videoPreviewLayer.connection.videoOrientation = AVCaptureVideoOrientationPortraitUpsideDown;
+    }
+}
+
+- (void)refreshARAnnotationViewsTimerTimeout:(NSTimer *)timer {
+    double viewBearing = BLSDegreesToRadians(self.locationManager.heading.trueHeading);
+    double fov = [self fieldOfView];
+    double minBearing = BLSNormalizeRadians(viewBearing - fov/2);
+    double maxBearing = BLSNormalizeRadians(minBearing+fov);
+    CLLocation *currentLocation = self.locationManager.location;
+    CLLocationCoordinate2D currentCoordinate = currentLocation.coordinate;
+    
+    for (id <BLSAugmentedAnnotation>annotation in self.annotations) {
+        double bearing = BLSBearingBetweenCoordinates(currentCoordinate, annotation.coordinate);
+        
+        BLSAugmentedAnnotationView *view = [self visibleViewForAnnotation:annotation];
+        
+        if ((minBearing < maxBearing && (bearing > minBearing && bearing < maxBearing)) ||
+            (minBearing > maxBearing && (bearing > minBearing || bearing > maxBearing))) {
+            
+            if (view == nil) {
+                view = [self.delegate augmentedViewController:self viewForAnnotation:annotation forUserLocation:currentLocation];
+                [self.vrView addSubview:view];
+                [self.viewsInUse addObject:view];
+                NSLog(@"Added view: %@", view);
+            }
+            
+            CGPoint centerPoint = self.vrView.center;
+            
+            centerPoint.x = BLSNormalizeRadians(bearing - minBearing) / fov * CGRectGetWidth(self.vrView.bounds);
+            centerPoint = BLSCGPointAdd(centerPoint, view.centerOffset);
+            
+            view.center = centerPoint;
+            
+            NSLog(@"centerPoint: %@", NSStringFromCGPoint(centerPoint));
+            
+        } else if (view != nil) {
+            [view removeFromSuperview];
+            [self.viewsInUse removeObjectIdenticalTo:view];
+            [self.reusableViews addObject:view];
+            NSLog(@"Removed view: %@", view);
+        }
+    }
+}
+
+- (BLSAugmentedAnnotationView *)visibleViewForAnnotation:(id<BLSAugmentedAnnotation>)annotation {
+    for (BLSAugmentedAnnotationView *view in self.viewsInUse) {
+        if ([view.annotation isEqual:annotation]) {
+            return view;
+        }
+    }
+    return nil;
+}
+
+- (double)fieldOfView {
+    double fov = BLSDegreesToRadians(self.captureDevice.activeFormat.videoFieldOfView);
+    
+    //TODO: calculate cropped fov
+    
+    return fov;
 }
 
 #pragma mark - Adding or removing annotations
@@ -210,8 +362,10 @@
 
 - (void)removeAnnotation:(id<BLSAugmentedAnnotation>)annotation {
     NSParameterAssert([annotation conformsToProtocol:@protocol(BLSAugmentedAnnotation)]);
-    if ([self.annotations indexOfObject:annotation] != NSNotFound) {
-        [self.annotations removeObject:annotation];
+    
+    NSUInteger index = [self.annotations indexOfObject:annotation];
+    if (index != NSNotFound) {
+        [self.annotations removeObjectAtIndex:index];
         [self.mapView removeAnnotation:annotation];
     }
 }
@@ -234,8 +388,24 @@
 
 #pragma mark - CLLocationManagerDelegate
 
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
-    
+//- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading {
+//    [self refreshARAnnotationsWithViewBearing:BLSDegreesToRadians(newHeading.trueHeading)];
+//}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse || status == kCLAuthorizationStatusAuthorizedAlways) {
+        [manager startUpdatingLocation];
+        [manager startUpdatingHeading];
+    }
+}
+
+#pragma mark - Rotation
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    if (self.style == BLSAugmentedViewControllerStyleVR) {
+        self.videoPreviewLayer.frame = CGRectMake(0, 0, size.width, size.height);
+        [self configureVideoOrientation];
+    }
 }
 
 @end
